a4dce743584b66278db13ac3227f75d3386bd79d
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-26 17:24:32 -0300

    alteracao no campo data no formulario

--- pessoa/forms.py
+++ pessoa/forms.py
@@ -5,6 +5,12 @@ from .models import Pessoa
 
 
 class PessoaForm(forms.ModelForm):
+    data_nascimento = forms.DateField(
+        widget=forms.TextInput(
+            attrs={"type": "date"}
+        )
+    )
+    
     class Meta:
         model = Pessoa
         fields = [
