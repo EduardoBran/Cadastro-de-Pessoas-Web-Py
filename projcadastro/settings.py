7c0ccfa28d2915ac5faea674da58a5a9a6fc5c63
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-26 17:15:23 -0300

    melhoria layout do formulario

--- projcadastro/settings.py
+++ projcadastro/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'pessoa',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -129,6 +130,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
