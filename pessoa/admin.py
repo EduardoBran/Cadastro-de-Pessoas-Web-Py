a7a674adc221e7415d381989b924f43e088a5f85
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-27 19:36:44 -0300

    configurando o admin

--- pessoa/admin.py
+++ pessoa/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pessoa
+
+
+# editando a ação
+@admin.action(description='Ativar todas as Pessoas')
+def ativar_todos(modeladmin, request, queryset):
+    queryset.update(ativo=True)
+
+@admin.action(description='Desativar todas as Pessoas')
+def desativar_todos(modeladmin, request, queryset):
+    queryset.update(ativo=False)
+
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome',
+        'sobrenome',
+        'data_nascimento',
+        'ativo'
+    ]
+    
+    list_filter = [
+        'ativo',
+        'data_nascimento'
+    ]
+    
+    search_fields = [
+        'nome'
+    ]
+
+    actions = [
+        ativar_todos,
+        desativar_todos
+    ]
+
+admin.site.register(Pessoa, PessoaAdmin)
