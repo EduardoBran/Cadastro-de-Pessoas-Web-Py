0d80ad1207d8bcd1108821a12464881a642db353
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-08-03 14:57:23 -0300

    colocando chave estrangeira

--- projcadastro/settings.py
+++ projcadastro/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-izzxpqa%q=r&cu#0mjfuia12a%rtpmxcfz!(uaukmdc6@d#l($'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
